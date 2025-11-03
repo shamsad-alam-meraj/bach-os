0d1569f0318d130d4d15e087805a197b15251b4c
Shamsad Alam Meraj <shamsadalammmeraj9@outlook.com>
2025-11-03 17:14:26 +0600

    deposit filters added

--- src/utils/baseUrl.ts
+++ src/utils/baseUrl.ts
@@ -1,2 +1,2 @@
-// export const BASE_URL = 'https://bachos-api.onrender.com/';
-export const BASE_URL = 'http://localhost:4000/';
+export const BASE_URL = 'https://bachos-api.onrender.com/';
+// export const BASE_URL = 'http://localhost:4000/';
